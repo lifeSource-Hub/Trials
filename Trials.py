23c611425e5a95b4477eaea9381c6b435b31e0c0
lifeSource <jd90craigslist@gmail.com>
2024-05-15 00:44:42 -0500

    Add info to ref txt file, code cleanup

--- Trials.py
+++ Trials.py
@@ -1,14 +1,18 @@
 import sys
 import Mutators
 
-GAME_MODE = 12
+GAME_MODE = None
 
 old_new_game = sys.modules["__main__"].PyGameView.new_game
 
 def new_game_redux(self):
-    mutators = Mutators.all_trials[GAME_MODE].mutators
-    trial_name = Mutators.all_trials[GAME_MODE].name
-    print(trial_name, mutators)
+    mutators = None
+    trial_name = None
+
+    if GAME_MODE is not None:
+        mutators = Mutators.all_trials[GAME_MODE].mutators
+        trial_name = Mutators.all_trials[GAME_MODE].name
+        print(trial_name, mutators)
 
     old_new_game(self, mutators=mutators, trial_name=trial_name, seed=None)
 
